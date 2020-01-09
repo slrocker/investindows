ae9ba314fb36d3f96e0bbed900a1344706d4e06c
slrocker <slrocker@gmail.com>
2020-01-09 12:28:55 -0200

    adsense try 2

--- src/components/adsense.js
+++ src/components/adsense.js
@@ -6,7 +6,7 @@ import * as React from "react";
 export default ({client, slot, format = 'auto'}) => (
     <div>
     <ins className="adsbygoogle"
-    css={{display: 'block'}}
+    
     data-ad-client={client}
     data-ad-slot={slot}
     data-ad-format={format}
